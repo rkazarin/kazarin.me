65cde2a51bbb4021aee5e4d247f9a345d49f974c
Roman Kazarin <rkazarin@umail.ucsb.edu>
2016-02-21 22:07:44 -0800

    updating picture, header, projects, resume

--- static/app.js
+++ static/app.js
@@ -1,1 +1,14 @@
-app.js
+var myApp = angular.module('mainModule', []);
+
+myApp.controller('kazarin_controller', ["$scope", "$window", "$http", function($scope, $window, $http) {
+
+    console.log("Initializing kazarin_controller");
+
+    $scope.popup = function() {
+    	window.alert("This code is private for academic reasons. Connect with me, and I can tell you all about it!")
+	}
+	$scope.popup2 = function() {
+    	window.alert("This code is private as it is an ongoing research project. Connect with me, and I can tell you all about it!")
+	}
+
+}]);
